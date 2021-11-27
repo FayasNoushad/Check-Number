6d87bb701d921b1d10ea63ab1b5d937ce0d7fbc1
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-27 17:15:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,15 +20,12 @@ def readme(file="README.md"):
 
 setuptools.setup(
     name="Check-Number",
-    version="1.0.2",
+    version="1.0.3",
     description="Number Checker",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/FayasNoushad/Check-Number",
-    download_url="https://github.com/FayasNoushad/Check-Number/releases/latest",
     license="MIT",
     author="Fayas Noushad",
-    author_email="contact@fayas.me",
     classifiers=[
         "Intended Audience :: Developers",
         "Natural Language :: English",
@@ -38,10 +35,10 @@ setuptools.setup(
     ],
     project_urls={
         "Tracker": "https://github.com/FayasNoushad/Check-Number/issues",
-        "Source": "https://github.com/FayasNoushad/Check-Number",
-        "Documentation": "https://check-number.projects.fayas.me",
+        "Source": "https://github.com/FayasNoushad/Check-Number"
     },
     python_requires=">=3.6",
+    py_modules=['check_number'],
     packages=setuptools.find_packages(),
     zip_safe=False,
     install_requires=requirements()
